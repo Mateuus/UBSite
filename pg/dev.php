ed1d5f7a629a3f9edd8368b12ba9fbcb295efe3b
Mateuus <mateuus27@outlook.com>
2016-06-06 06:04:31 -0300

    Add Paginação Configuração do template Add novas paginas

---
+++ pg/dev.php
@@ -0,0 +1,12 @@
+﻿<div class="box">
+            
+            	<!-- TOURNAMENT BRACKET - START -->
+  <div class="tournament-bracket hidden-xs">
+                	<h2>CONSTRUÇÃO...</h2>
+                	<div class="row">
+              	 <div class="box servers">
+                        <div class="table-responsive"></div>	
+              </div>
+        </div>
+   </div>
+</div>  
